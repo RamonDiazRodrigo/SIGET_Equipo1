4c68226450225d9c954e5324478d2e550fb8831b
Aharon1377 <aharon1377@gmail.com>
2020-10-14 10:13:48 +0200

    Crear Proyecto Spring v2

---
+++ src/main/java/com/app/SIGET/SigetEquipo1Application.java
@@ -0,0 +1,13 @@
+package com.app.SIGET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigetEquipo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigetEquipo1Application.class, args);
+	}
+
+}
