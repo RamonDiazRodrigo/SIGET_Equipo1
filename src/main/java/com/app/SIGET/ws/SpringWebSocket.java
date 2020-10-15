809cd85e7cf24c6a9c615b45536606090caa52b4
Aharon1377 <aharon1377@gmail.com>
2020-10-15 18:39:28 +0200

    crear metodos websocket login y register

--- src/main/java/com/app/SIGET/ws/SpringWebSocket.java
+++ src/main/java/com/app/SIGET/ws/SpringWebSocket.java
@@ -12,33 +12,30 @@ import com.app.SIGET.dominio.Manager;
 @Component
 public class SpringWebSocket extends TextWebSocketHandler {
 
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        
-    }
-    
-    @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        JSONObject jso = new JSONObject(message.getPayload().toString());
-        if (jso.getString("type").equals("ready")) {
-            session.sendMessage(new TextMessage(Manager.get().leer().toString()));
-        }
+	private static final String NOMBRE = "nombre";
+	
+	@Override
+	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		JSONObject jso = new JSONObject(message.getPayload().toString());
+		if (jso.getString("type").equals("ready")) {
+			session.sendMessage(new TextMessage(Manager.get().leer().toString()));
+		}
 
-        if(jso.getString("type").equals("insertar")){
-        	Manager.get().insertar((String)jso.get("nombre"));
-        	session.sendMessage(new TextMessage(Manager.get().leer().toString()));
-        }
-        
-        if(jso.getString("type").equals("actualizar")){
-        	Manager.get().actualizar((String)jso.get("nombre"),jso.getBoolean("done") );
-        	session.sendMessage(new TextMessage(Manager.get().leer().toString()));
-        }
+		if (jso.getString("type").equals("insertar")) {
+			Manager.get().insertar((String) jso.get(NOMBRE));
+			session.sendMessage(new TextMessage(Manager.get().leer().toString()));
+		}
 
-        if(jso.getString("type").equals("eliminar")){
-        	Manager.get().eliminar((String)jso.get("nombre"));
-        	session.sendMessage(new TextMessage(Manager.get().leer().toString()));
-        }
-        
-    }
-  
+		if (jso.getString("type").equals("actualizar")) {
+			Manager.get().actualizar((String) jso.get(NOMBRE), jso.getBoolean("done"));
+			session.sendMessage(new TextMessage(Manager.get().leer().toString()));
+		}
+
+		if (jso.getString("type").equals("eliminar")) {
+			Manager.get().eliminar((String) jso.get(NOMBRE));
+			session.sendMessage(new TextMessage(Manager.get().leer().toString()));
+		}
+
+	}
+
 }
