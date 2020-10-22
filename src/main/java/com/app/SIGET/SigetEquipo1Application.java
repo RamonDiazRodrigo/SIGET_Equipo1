8ee648ed3a899b9e45ec07b1a4ba9aab3bab8c9c
alber <alber@DESKTOP-74PAG30>
2020-10-22 11:06:11 +0200

    Prueba 2 JQuery

--- src/main/java/com/app/SIGET/SigetEquipo1Application.java
+++ src/main/java/com/app/SIGET/SigetEquipo1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.app.SIGET.http"})
 public class SigetEquipo1Application {
 
 	public static void main(String[] args) {
