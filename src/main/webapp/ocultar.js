684a4e3fcc17199ca075404e80f6dd7b7b037cc3
davidc7910 <51422386+davidc7910@users.noreply.github.com>
2020-11-04 13:13:49 +0100

    convocar reuniones vista usuario

--- src/main/webapp/ocultar.js
+++ src/main/webapp/ocultar.js
@@ -10,3 +10,16 @@ $('#volver').click(function(e) {
 	 $('#hide-me').css('visibility', 'hidden');
 	 $('#visibilityHidden').css('visibility', 'visible');
 });
+
+$('#Reunion').click(function(e) {
+	  if( $('#hide_reunion').css('visibility') != 'hidden' ) {
+	    $('#hide_reunion').css('visibility', 'hidden');
+	  } else {
+	    $('#hide_reunion').css('visibility', 'visible');
+	  }
+	  $('#Reunion').css('visibility', 'hidden');
+	});
+	$('#volverReunion').click(function(e) {
+		 $('#hide_reunion').css('visibility', 'hidden');
+		 $('#Reunion').css('visibility', 'visible');
+	});
