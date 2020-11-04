45abea2a4bef0d9e074d8d847eaef5ac04712035
Aharon1377 <aharon1377@gmail.com>
2020-11-04 09:21:21 +0100

    Crear feature y caso de prueba en HDU5

--- src/test/java/com/app/SIGET/cucumber/pruebas/CasoDePrueba.java
+++ src/test/java/com/app/SIGET/cucumber/pruebas/CasoDePrueba.java
@@ -123,5 +123,21 @@ public class CasoDePrueba {
 	public void veo_la_agenda_del(String usuario) throws Throwable {
 		Manager.get().leerActividades(usuario);
 	}
+	
+	@Given("^\"([^\"]*)\", \"([^\"]*)\",  \"([^\"]*)\",  \"([^\"]*)\",  \"([^\"]*)\",  \"([^\"]*)\" y \"([^\"]*)\" involucrados$")
+	public void y_involucrados(String nombre, String dia, String horaI, String minutosI, String horaF,
+			String minutosF, String usuarios) throws NumberFormatException {
+		dia="LUNES";
+		horaI="15";
+		minutosI="00";
+		horaF="16";
+		minutosF="30";
+		usuarios= new JSONArray().toString();
+		Manager.get().convocarReunion(nombre, dia, horaI, minutosI, horaF, minutosF, usuarios, "true");
+	}
+
+	@Then("^Crea una reunion \"([^\"]*)\" y la aniade a reuniones pendientes si no tienen ese horario ocupado$")
+	public void crea_una_reunion_y_la_aniade_a_reuniones_pendientes_si_no_tienen_ese_horario_ocupado(String arg1) throws Throwable {
+	}
 
 }
