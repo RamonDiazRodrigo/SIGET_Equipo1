e2f98eb695c856d8f54532921e7e0c9d85f17908
RamonDiazRodrigo <Ramon.Diaz2@alu.uclm.es>
2020-12-09 00:13:29 +0100

    primer cambio

--- src/test/java/com/app/siget/cucumber/pruebas/CasoDePruebaAsistirReuniones.java
+++ src/test/java/com/app/siget/cucumber/pruebas/CasoDePruebaAsistirReuniones.java
@@ -1,39 +1,32 @@
 package com.app.siget.cucumber.pruebas;
 
-import org.json.JSONArray;
-
 import com.app.siget.dominio.Manager;
+import com.app.siget.excepciones.FranjaHorariaOcupadaException;
+import com.app.siget.persistencia.UserDAO;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class CasoDePruebaAsistirReuniones {
 
-	@Given("^\"([^\"]*)\", \"([^\"]*)\",  \"([^\"]*)\",  \"([^\"]*)\",  \"([^\"]*)\",  \"([^\"]*)\" y un \"([^\"]*)\"$")
-	public void y_un(String nombre, String dia, String horaI, String minutosI, String horaF, String minutosF,
-			String usuario) throws Throwable {
-		nombre="asistente";
-		dia = "LUNES";
-		horaI = "15";
-		minutosI = "00";
-		horaF = "16";
-		minutosF = "30";
-		usuario = new JSONArray().toString();
+	@Given("^el usuario \"([^\"]*)\" acepta la reunion \"([^\"]*)\"$")
+	public void el_usuario_acepta_la_reunion(String nombre, String id) throws FranjaHorariaOcupadaException {
+		Manager.get().aceptarReunion(nombre, Integer.parseInt(id));
 	}
 
-	@Then("^el \"([^\"]*)\" acepta la reunion \"([^\"]*)\" y aparece en su agenda$")
-	public void el_acepta_la_reunion_y_aparece_en_su_agenda(String usuario, String id) throws Throwable {
-		id="1";
-		usuario="asistente";
-		Manager.get().aceptarReunion(usuario, Integer.parseInt(id));
+	@Then("^el usuario \"([^\"]*)\" tiene la reunion \"([^\"]*)\" en su agenda$")
+	public void el_usuario_tiene_la_reunion_en_su_agenda(String usuario, String id) throws Throwable {
+		UserDAO.findUser(usuario)
 	}
 
-	@Then("^el \"([^\"]*)\" acepta la reunion \"([^\"]*)\" y no aparece en su agenda$")
-	public void el_acepta_la_reunion_y_no_aparece_en_su_agenda(String usuario, String id) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		id="1";
-		usuario="asistente";
-		Manager.get().rechazarReunion(usuario, Integer.parseInt(id));
+	@Given("^el usuario \"([^\"]*)\" acepta la reunion \"([^\"]*)\"$")
+	public void el_usuario_rechaza_la_reunion(String nombre, String id) throws FranjaHorariaOcupadaException {
+		Manager.get().rechazarReunion(nombre, Integer.parseInt(id));
+	}
+
+	@Then("^el usuario \"([^\"]*)\" tiene la reunion \"([^\"]*)\" en su agenda$")
+	public void el_usuario_no_tiene_la_reunion_en_su_agenda(String usuario, String id) throws Throwable {
+		
 	}
 
 }
