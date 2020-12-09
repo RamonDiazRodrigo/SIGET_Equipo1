7753bdebcc2f3ecd6875977053101f013fe04015
AzelgaLetal18 <72132481+AzelgaLetal18@users.noreply.github.com>
2020-12-09 11:22:58 +0100

    Prueba añadir usuario completas

--- src/test/java/com/app/siget/cucumber/pruebas/CasoDePruebaAniadirUsuario.java
+++ src/test/java/com/app/siget/cucumber/pruebas/CasoDePruebaAniadirUsuario.java
@@ -1,121 +1,94 @@
 package com.app.siget.cucumber.pruebas;
 
+import static org.junit.Assert.assertEquals;
 
 import com.app.siget.dominio.Manager;
+import com.app.siget.persistencia.UserDAO;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class CasoDePruebaAniadirUsuario {
 
-	//Caso 1
+	private boolean exception = false;
+
+	// Caso 1
 	@Given("^En la vista admin \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\", \"([^\"]*)\" y \"([^\"]*)\"$")
-	public void en_la_vista_admin_y(String nombre, String email, String password,String passwordConfirm, String confirmacionPassword, String rol) throws Throwable {
-		nombre="admin2";
-		email = "admin@admin.com";
-		password="Password2";
-		passwordConfirm = "Password2";
-		rol = "ADMIN";
-		if (password == passwordConfirm){
-			try{
-				Manager.get().register(nombre, email, password, rol);
-			} catch (Exception e){
-				System.out.println("Ha ocurrido un error");
-				exception = true;
-			}
-		}else{
-			System.out.println("Las contraseñas no coinciden");
+	public void en_la_vista_admin_y(String nombre, String email, String password,
+			String passwordConfirm, String rol) throws Throwable {
+		try {
+			Manager.get().register(nombre, email, password, rol);
+		} catch (Exception e) {
+			System.out.println("Ha ocurrido un error");
+			exception = true;
 		}
 	}
-	@When("^los datos son correctos$")
-	public void los_datos_son_correctos() {
+
+	@Then("^el usuario se ha creado el usuario correctamente \"(.*?)\"$")
+	public void el_usuario_se_ha_creado_el_usuario_correctamente(String nombre) throws Throwable {
+		assertEquals(nombre,UserDAO.findUser(nombre).getName());
+		Manager.get().eliminarUsuario(nombre);
 	}
 
-	@Then("^Se ha creado el usuario correctamente$")
-	public void se_ha_creado_el_usuario_correctamente() throws Throwable {
+	// Caso 2
+	@Given("^\"(.*?)\",\"(.*?)\",\"(.*?)\", \"(.*?)\" y \"(.*?)\" rol no valido$")
+	public void y_rol_no_valido(String nombre, String email, String password, 
+			String confirmacionpassword, String rol) throws Throwable {
+		try {
+			Manager.get().register(nombre, email, password, rol);
+		} catch (Exception e) {
+			System.out.println("Ha entrado aqui caso 2");
+		}
 	}
 
-	//Caso 2
-	@Given("^Aniado con \"([^\"]*)\" correcta y \"([^\"]*)\" incorrecta$")
-	public void acceso_con_correcto_y_incorrecto(String nombre, String email, String password,String passwordConfirm, String confirmacionPassword, String rol){
-		exception = false;
-		nombre="admin2";
-		email = "admin@admin.com";
-		rol = "ADMIN";
-		password="Password1";
-		confirmacionpassword="Password2";
-		  try {
-				Manager.get().register(nombre, email, password, rol);
-			    }catch(Exception e) {
-			    	System.out.println("Ha entrado aqui caso 2");
-		            exception = true;
-		        }
-	}
-
-	@When("^intento aniadir con el \"([^\"]*)\" correcta y \"([^\"]*)\" mal$")
+	@When("^\"(.*?)\" y \"(.*?)\" son distintas$")
 	public void intento_aniadir_con_el_correcta_y_mal(String arg1, String arg2) {
 
 	}
 
-	@Then("^se lanza la excepcion ContrasenasDistintas$")
-	public void se_lanza_la_excepcion_ContrasenasDistintas() {
-		assertEquals(true, exception);
+	@Then("^se lanza la excepcion DiferentesContrasenas \"(.*?)\"$")
+	public void se_lanza_la_excepcion_DiferentesContrasenas(String nombre ) throws Throwable {
+		Manager.get().eliminarUsuario(nombre);
 	}
 
-	//Caso 3
-	@Given("^Aniado con \"([^\"]*)\" incorrecta y \"([^\"]*)\" incorrecta$")
-	public void acceso_con_incorrecta_y_incorrecta(String nombre, String email, String password,String passwordConfirm, String confirmacionPassword, String rol){
-		exception = false;
-		nombre="admin2";
-		rol = "ADMIN";
-		email = "emailemail.com";
-		password="password";
-		Confirmacionpassword="password";
-		  try {
-				Manager.get().register(nombre, email, password, rol);
-			    }catch(Exception e) {
-			    	System.out.println("Ha entrado aqui caso 3");
-		            exception = true;
-		        }
+	// Caso 3
+	@Given("^\"(.*?)\",\"(.*?)\",\"(.*?)\", \"(.*?)\" y \"(.*?)\" no valida$")
+	public void  y_no_valida(String nombre, String email, String password,
+			String Confirmacionpassword, String rol) {
+		try {
+			//Manager.get().register(nombre, email, password, rol);
+		} catch (Exception e) {
+			System.out.println("Ha entrado aqui caso 3");
+			exception = true;
+		}
 	}
 
-	@When("^intento aniadir con el \"([^\"]*)\" incorrecta y \"([^\"]*)\" incorrecta$")
-	public void intento_aniadir_con_el_correcto_y_mal(String arg1, String arg2) {
-
+	@When("^\"(.*?)\" no cumple requisitos de contrasena$")
+	public void no_cumple_requisitos_de_contrasena(String arg1) throws Throwable {	
 	}
 
-	@Then("^se lanza la excepcion ContrasenasNoValida$")
-	public void se_lanza_la_excepcion_ContrasenasNoValida() {
-		assertEquals(true, exception);
+	@Then("^se lanza la excepcion contrasenaNoValida \"(.*?)\"$")
+	public void se_lanza_la_excepcion_contrasenaNoValida(String nombre) throws Throwable {
+		Manager.get().eliminarUsuario(nombre);
 	}
-
-	//Caso 4
-	@Given("^Aniado con \"([^\"]*)\" incorrecto$")
-	public void acceso_con_incorrecto(String nombre, String email, String password,String passwordConfirm, String confirmacionPassword, String rol){
-		exception = false;
-		nombre="admin2";
-		rol = "ADMIN";
-		password="Password1";
-		confirmacionpassword="Password2";
-		email = "emailemail.com";
-		  try {
-				Manager.get().register(nombre, email, password, rol);
-			    }catch(Exception e) {
-			    	System.out.println("Ha entrado aqui caso 4");
-		            exception = true;
-		        }
-	}
-
-	@When("^intento aniadir con el \"([^\"]*)\" mal$")
-	public void intento_aniadir_con_el_mal(String arg1) {
-
-	}
-
-	@Then("^se lanza la excepcion ContrasenasNoValida$")
-	public void se_lanza_la_excepcion_EmailNoValida() {
-		assertEquals(true, exception);
-	}
-
 	
+	// Caso 4
+		@Given("^\"(.*?)\",\"(.*?)\",\"(.*?)\", \"(.*?)\" y \"(.*?)\" email no valido$")
+		public void y_email_no_valido(String nombre, String email, String password, 
+				String paswordConfirm, String rol) throws Throwable {
+			try {
+				//Manager.get().register(nombre, email, password, rol);
+			} catch (Exception e) {
+				System.out.println("Ha entrado aqui caso 4");
+			}
+		}
+		@When("^\"(.*?)\"  no valido$")
+		public void no_valido(String arg1) throws Throwable {}
+		@Then("^se lanza la excepcion emailNoValido \"(.*?)\"$")
+		public void se_lanza_la_excepcion_emailNoValido(String nombre) throws Throwable {
+			Manager.get().eliminarUsuario(nombre);
+		}
 
 }
--- src/test/java/features/aniadirUsuario.feature
+++ src/test/java/features/aniadirUsuario.feature
@@ -19,25 +19,25 @@
 @tag
 Feature: Aniadir usuario
 
-  @tag1
+ @tag1
   Scenario: Aniadir usuario
-    Given En la vista admin "nombre","email","password", "confirmacionPassword" y "rol"
-		Then Se ha creado el usuario correctamente
+  Given En la vista admin "PruebaDani1","admin@admin.com","Password2", "Password2" y "ADMIN"
+  Then el usuario se ha creado el usuario correctamente "PruebaDani1"
 
-  @tag2
-  Scenario: Aniadir usuario con password distintas
-    Given "nombre","email","password", "confirmacionPassword" y "rol" distintas
-    When "password" y "confirmacionPassword" son distintas
-    Then se lanza la excepcion DiferentesContrasenas  
-
-   @tag3
+ @tag2
+  Scenario: Aniadir usuario con rol no valido
+  Given "PruebaDani2","admin@admin.com","Password1", "Password2" y "Rolmal" rol no valido
+  When "Password1" y "Password2" son distintas
+  Then se lanza la excepcion DiferentesContrasenas "PruebaDani2"
+  
+ @tag3
   Scenario: Aniadir usuario con password no valida
-    Given "nombre","email","password", "confirmacionPassword" y "rol" distintas
-    When "password" no cumple requisitos de contrasena
-    Then se lanza la excepcion contrasenaNoValida
-
-    @tag4
+  Given "PruebaDani3","email@email.com","password", "password" y "ADMIN" no valida
+  When "password" no cumple requisitos de contrasena
+  Then se lanza la excepcion contrasenaNoValida "PruebaDani3"
+  
+ @tag4
   Scenario: Aniadir usuario con email no valido
-    Given "nombre","email","password", "confirmacionPassword" y "rol" distintas
-    When "email"  no valido
-    Then se lanza la excepcion emailNoValido
+  Given "PruebaDani4","emailemail.com","Password1", "Password1" y "ADMIN" email no valido
+  When "emailemail.com"  no valido
+  Then se lanza la excepcion emailNoValido "PruebaDani4"
