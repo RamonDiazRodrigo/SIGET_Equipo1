39e55e840ca0f8acc86714f96f1db5dde4d06e8d
chemabr24 <JoseManuel.Bastante@alu.uclm.es>
2020-12-11 20:08:53 +0100

    Prueba añadir usuario mejorada

--- src/test/java/com/app/siget/cucumber/pruebas/CasoDePruebaAniadirUsuario.java
+++ src/test/java/com/app/siget/cucumber/pruebas/CasoDePruebaAniadirUsuario.java
@@ -1,31 +1,23 @@
 package com.app.siget.cucumber.pruebas;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+import java.util.ArrayList;
 import com.app.siget.dominio.Manager;
+import com.app.siget.dominio.User;
 import com.app.siget.persistencia.UserDAO;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import com.app.siget.dominio.Manager;
-import com.app.siget.excepciones.DiferentesContrasenasException;
-
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
 public class CasoDePruebaAniadirUsuario {
 
 	// Caso 1
-	@Given("^En la vista admin \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\", \"([^\"]*)\" y \"([^\"]*)\"$")
+	@Given("^En la vista admin \"([^\"])\",\"([^\"])\",\"([^\"])\", \"([^\"])\" y \"([^\"]*)\"$")
 	public void en_la_vista_admin_y(String nombre, String email, String password,
 			String passwordConfirm, String rol) throws Throwable {
-		try {
 			Manager.get().register(nombre, email, password, rol);
-		} catch (Exception e) {
-			System.out.println("Ha ocurrido un error");
-		}
+	
 	}
 
 	@Then("^el usuario se ha creado el usuario correctamente \"(.*?)\"$")
@@ -34,62 +26,27 @@ public class CasoDePruebaAniadirUsuario {
 		Manager.get().eliminarUsuario(nombre);
 	}
 
-	// Caso 2
-	@Given("^\"(.*?)\",\"(.*?)\",\"(.*?)\", \"(.*?)\" y \"(.*?)\" rol no valido$")
-	public void y_rol_no_valido(String nombre, String email, String password, 
-			String confirmacionpassword, String rol) throws Throwable {
-		try {
-			Manager.get().register(nombre, email, password, rol);
-		} catch (Exception e) {
-			System.out.println("Ha entrado aqui caso 2");
-		}
-	}
 
-	@When("^\"(.*?)\" y \"(.*?)\" son distintas$")
-	public void intento_aniadir_con_el_correcta_y_mal(String arg1, String arg2) {
 
-	}
-
-	@Then("^se lanza la excepcion DiferentesContrasenas \"(.*?)\"$")
-	public void se_lanza_la_excepcion_DiferentesContrasenas(String nombre ) throws Throwable {
-		Manager.get().eliminarUsuario(nombre);
-	}
-
-	// Caso 3
-	@Given("^\"(.*?)\",\"(.*?)\",\"(.*?)\", \"(.*?)\" y \"(.*?)\" no valida$")
-	public void  y_no_valida(String nombre, String email, String password,
-			String Confirmacionpassword, String rol) {
-		try {
-			//Manager.get().register(nombre, email, password, rol);
-		} catch (Exception e) {
-			System.out.println("Ha entrado aqui caso 3");
-		}
-	}
-
-	@When("^\"(.*?)\" no cumple requisitos de contrasena$")
-	public void no_cumple_requisitos_de_contrasena(String arg1) throws Throwable {	
-	}
-
-	@Then("^se lanza la excepcion contrasenaNoValida \"(.*?)\"$")
-	public void se_lanza_la_excepcion_contrasenaNoValida(String nombre) throws Throwable {
-		Manager.get().eliminarUsuario(nombre);
+	
+	@Then("^no se ha registrado el usuario vacio \"(.*?)\"$")
+	public void no_se_ha_registrado_el_usuario_vacio(String nombre) throws Throwable {
+		assertNotEquals(nombre,UserDAO.findUser(nombre).getName());
+		
 	}
 	
-	// Caso 4
-		@Given("^\"(.*?)\",\"(.*?)\",\"(.*?)\", \"(.*?)\" y \"(.*?)\" email no valido$")
-		public void y_email_no_valido(String nombre, String email, String password, 
-				String paswordConfirm, String rol) throws Throwable {
-			try {
-				//Manager.get().register(nombre, email, password, rol);
-			} catch (Exception e) {
-				System.out.println("Ha entrado aqui caso 4");
-			}
+	
+	@Then("^no se registrado el usuario \"(.*?)\"$")
+	public void sno_se_ha_registrado_el_usuario(String nombre) throws Throwable {
+		int repeticiones=0;
+		ArrayList<User> listausers= (ArrayList) UserDAO.leerUsers();
+		for(int i=0; i<listausers.size();i++) {
+			if(nombre.equals(listausers.get(i).getName()))
+				repeticiones ++;
 		}
-		@When("^\"(.*?)\"  no valido$")
-		public void no_valido(String arg1) throws Throwable {}
-		@Then("^se lanza la excepcion emailNoValido \"(.*?)\"$")
-		public void se_lanza_la_excepcion_emailNoValido(String nombre) throws Throwable {
-			Manager.get().eliminarUsuario(nombre);
-		}
+		assertEquals(1,repeticiones);
+
+	}
+		
 
 }
